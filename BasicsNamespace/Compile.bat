eddcaf9e1897b136c11d663cd81bc12bfc275099
Jerhynh <60614943+Jerhynh@users.noreply.github.com>
2020-11-27 04:03:06 -0600

    Pushing Initial Files

---
+++ BasicsNamespace/Compile.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Beginning Compile of main.cpp. Sit tight!
+echo [Task]: Running Command: g++ -Wall -std=c++17 main.cpp -o main
+g++ -Wall -std=c++17 main.cpp -o main
+
+echo Finished!
---
+++ BasicsNamespace/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Remember: Always save prior to compiling!
+
+int main() {
+    std::cout << "Hello Windows command line!" << std::endl;
+    std::cout << "Hello World I am compiled via g++" << std::endl;
+    return 0;
+}
+

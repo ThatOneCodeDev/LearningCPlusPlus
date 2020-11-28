4bf43968e1daa8ada7267b3859a4f113ce27ea96
Jerhynh <60614943+Jerhynh@users.noreply.github.com>
2020-11-27 22:42:48 -0600

    Finished up ChapterTwoNameSpace...

---
+++ ChapterTwoNamespace/C++ Structures/main.cpp
@@ -0,0 +1,34 @@
+// Section 5: Basic I/O using cin and cout
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello, code in this project has been commented out during testing feel free to un-comment it!" << endl;
+
+    // cout << "Hello";
+    // cout << "World" << endl;
+    // cout << "Hello World!" << endl;
+    // cout << "Hello" << " World!" << endl;
+    // cout << "Hello" << " World!\n";
+    // cout << "Hello\nOut\nThere\n";
+
+    // int num1, num2;
+    // double num3;
+
+    // cout << "Enter an Integer: ";
+    // cin >> num1;
+    // cout << "You entered: " << num1 << endl;
+
+    // cout << "enter a first integer: ";
+    // cin >> num1;
+
+    // cout << "Enter two integers separated by a space: ";
+    // cin >> num1 >> num2;
+    // cout << "You entered " << num1 << " and " << num2 << endl;
+
+    // cout << "Enter a double: ";
+    // cin >> num3;
+    // cout << "you entered: " << num3 << endl;
+
+}
